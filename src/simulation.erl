d2ad727de6b2637255fedf9918424cf54b3ad856
Wojciech Gawronski <afronski@gmail.com>
2015-05-16 16:06:03 +0200

    Lowering amount of wolves by default.

--- src/simulation.erl
+++ src/simulation.erl
@@ -9,7 +9,7 @@ read_world_parameters_from_settings() ->
     Carrots = application:get_env(wolves_and_rabbits_world_simulation, carrots, 60),
 
     Rabbits = application:get_env(wolves_and_rabbits_world_simulation, rabbits, 12),
-    Wolves = application:get_env(wolves_and_rabbits_world_simulation, wolves, 5),
+    Wolves = application:get_env(wolves_and_rabbits_world_simulation, wolves, 3),
 
     Width = application:get_env(wolves_and_rabbits_world_simulation, width, 15),
     Height = application:get_env(wolves_and_rabbits_world_simulation, height, 15),
