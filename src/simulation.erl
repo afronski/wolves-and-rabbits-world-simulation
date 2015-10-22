245cbff5a87f3cae0f4cc7bc422e0a4d139fcecc
Wojciech Gawronski <afronski@gmail.com>
2015-10-22 19:12:58 +0200

    Restoring previous defaults.

--- src/simulation.erl
+++ src/simulation.erl
@@ -6,13 +6,13 @@
 -export([ start/2, stop/1 ]).
 
 read_world_parameters_from_settings() ->
-    Carrots = application:get_env(wolves_and_rabbits_world_simulation, carrots, 60),
+    Carrots = application:get_env(wolves_and_rabbits_world_simulation, carrots, 200),
 
-    Rabbits = application:get_env(wolves_and_rabbits_world_simulation, rabbits, 12),
-    Wolves = application:get_env(wolves_and_rabbits_world_simulation, wolves, 3),
+    Rabbits = application:get_env(wolves_and_rabbits_world_simulation, rabbits, 50),
+    Wolves = application:get_env(wolves_and_rabbits_world_simulation, wolves, 15),
 
-    Width = application:get_env(wolves_and_rabbits_world_simulation, width, 15),
-    Height = application:get_env(wolves_and_rabbits_world_simulation, height, 15),
+    Width = application:get_env(wolves_and_rabbits_world_simulation, width, 70),
+    Height = application:get_env(wolves_and_rabbits_world_simulation, height, 70),
 
     #world_parameters{carrots = Carrots,
                       rabbits = Rabbits,
